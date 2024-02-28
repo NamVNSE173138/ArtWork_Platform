fa8f453f7bd1ba0f5c429fcf770c6692e0ba8232
Anh Quan <qsao2212@gmail.com>
2024-02-28 13:13:23 +0700

    ArtworkRequest

---
+++ src/page/ArtworkRequest.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "../components/Navbar/Navbar";
+import Mainboard from "../components/Mainboard/Mainboard";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import ArtworkRequest from "../components/ArtworkRequest/ArtworkRequest";
+
+interface Pin {
+  _id: string;
+  artworkId: number;
+  userId: number;
+  artworkName: string;
+  createTime: Date;
+  tags: string;
+  numOfLike: number;
+  price: string;
+  describe: string;
+  imageUrl: string;
+}
+
+interface ArtworkResponse {
+  data: Pin[];
+}
+
+const Home: React.FC = () => {
+  const [pins, setPins] = useState<Pin[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [image, setImage] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/artworks")
+      .then((response) => response.json())
+      .then((res) => {
+        setImage(res);
+        console.log(res);
+      });
+  }, []);
+  // console.log(image);
+
+  const getImages = async () => {
+    try {
+      const response = await axios.get<ArtworkResponse>(
+        "http://localhost:5000/artworks"
+      );
+      console.log("reponse: ", response);
+      console.log(response.data);
+
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching artwork:", error);
+      throw error;
+    }
+  };
+
+  const onSearchSubmit = async (term: string) => {
+    setLoading(true);
+
+    try {
+      const res = await getImages();
+      const newPins = Array.isArray(res.data) ? res.data : [];
+
+      newPins.sort(() => 0.5 - Math.random());
+      setPins(newPins);
+    } catch (error) {
+      console.error("Error fetching search images:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getNewPins = async () => {
+    setLoading(true);
+
+    try {
+      const res = await getImages();
+
+      // Check if res.data is defined and is an array before sorting
+      const pinData = Array.isArray(res.data) ? res.data : [];
+
+      setPins(pinData);
+    } catch (error) {
+      console.error("Error fetching new pins:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getNewPins();
+  }, []);
+
+  useEffect(() => {}, [pins]);
+  return (
+    <>
+      <Navbar onSubmit={onSearchSubmit} />
+      {loading ? (
+        <Spin indicator={<LoadingOutlined style={{ fontSize: 60 }} spin />} />
+      ) : (
+        <ArtworkRequest />
+      )}
+    </>
+  );
+};
+
+export default Home;
